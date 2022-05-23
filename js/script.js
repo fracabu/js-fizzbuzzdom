11159004fc2c0026ece033a7f01eab83b91f4aa6
fracabu <100199965+fracabu@users.noreply.github.com>
2022-05-23 19:22:26 +0200

    aggiunte parentesi

--- js/script.js
+++ js/script.js
@@ -12,22 +12,38 @@
 
 //inserire funzione per stampare numeri da 1 a 100 in console
 
-let text = i.toString()
+
 
 for (let i = 0; i <= 100; i++) {
-    if (i % 3 === 0 & i % 5 === 0) console.log("FizzBuzz");
-    else if (i % 3 === 0) console.log("Fizz");
-    else if (i % 5 === 0) console.log("Buzz");
-    else console.log(i);
+
+    console.log(i);
+
+
+    if (i % 3 === 0 & i % 5 === 0){
+        
+        console.log("FizzBuzz");
+
+    } else if (i % 3 === 0){
+        
+        console.log("Fizz");
+
+    } else if (i % 5 === 0){
+
+        console.log("Buzz");
+
+    } 
+    
+    
+
 
 }
 
-
+/*
 document.getElementById("box") 
 const squareBox = document.getElementById("box")
 squareBox.innerHTML = "box1" + "box2";
 
 
 document.getElementById("").addEventListener()
-
+*/
 
