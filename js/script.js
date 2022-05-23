54a9cf2692bbaf351bfb98ec94c2d6b6cdb029d6
fracabu <100199965+fracabu@users.noreply.github.com>
2022-05-23 17:39:00 +0200

    modifiche

--- js/script.js
+++ js/script.js
@@ -12,6 +12,8 @@
 
 //inserire funzione per stampare numeri da 1 a 100 in console
 
+let text = i.toString()
+
 for (let i = 0; i <= 100; i++) {
     if (i % 3 === 0 & i % 5 === 0) console.log("FizzBuzz");
     else if (i % 3 === 0) console.log("Fizz");
@@ -24,3 +26,8 @@ for (let i = 0; i <= 100; i++) {
 document.getElementById("box") 
 const squareBox = document.getElementById("box")
 squareBox.innerHTML = "box1" + "box2";
+
+
+document.getElementById("").addEventListener()
+
+
